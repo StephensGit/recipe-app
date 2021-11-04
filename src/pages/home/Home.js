94408aabddfaacaeb3e424b9a7bd370a53daa853
StephensGit <stephenmoran@live.ie>
2021-11-04 11:06:27 +0000

    Fetching data

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,8 +1,21 @@
+import { useFetch } from '../../hooks/UseFetch';
+import RecipeList from '../../components/RecipeList';
+// import styles
 import './Home.css';
 export default function Home() {
+  const {
+    data: recipes,
+    error,
+    isPending,
+  } = useFetch('http://localhost:3000/recipes');
+
+  console.log({ recipes });
+
   return (
     <div>
-      <h1>Home</h1>
+      {error && <p className='error'></p>}
+      {isPending && <p className='loading'>Loading...</p>}
+      {recipes && <RecipeList recipes={recipes} />}
     </div>
   );
 }
